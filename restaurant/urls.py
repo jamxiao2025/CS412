001ae78ff3fa344000aca4724844ddbc86d43415
James Xiao <95207937+jamxiao2025@users.noreply.github.com>
2024-09-30 20:57:39 -0400

    Update urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from . import views
 # all of the URLs that are part of this app
 urlpatterns = [
+    path(r'', views.main, name="main"),
     path(r'main/', views.main, name="main"),
     path(r'order/', views.order, name="order"),
     path(r'confirmation/', views.confirmation, name="confirmation")
